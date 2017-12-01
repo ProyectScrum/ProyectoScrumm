dc6cb716edb5af1387b81a82c305ad06e2c0a05b
kleer <kleer@kleer-csd-box>
2017-12-01 09:37:38 -0500

    Se Genero Historia de Usuario Welcome

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,1 +1,11 @@
+Given(/^que inicie la aplicacion$/) do
+    visit '/'
+end
 
+Then(/^debo ver "([^"]*)"$/) do |text|
+  expect(page.body).to match /#{text}/m
+end
+
+Given(/^le di click al boton "([^"]*)"$/) do |name|
+  click_button(name)
+end
